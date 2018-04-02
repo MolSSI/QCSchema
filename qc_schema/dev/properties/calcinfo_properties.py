d2d60107973a767a357b2c29fdbe6014632a40a7
Daniel Smith <malorian@me.com>
2018-04-02 11:45:39 -0400

    Adds in calcinfo properties

---
+++ qc_schema/dev/properties/calcinfo_properties.py
@@ -0,0 +1,45 @@
+"""
+The complete list of the general computation properties.
+"""
+
+calcinfo_properties = {}
+
+calcinfo_properties["calcinfo_nbasis"] = {
+    "type":
+    "number",
+    "multipleOf": 1.0,
+    "description":
+    "The number of basis functions for the computation."
+}
+
+calcinfo_properties["calcinfo_nmo"] = {
+    "type":
+    "number",
+    "multipleOf": 1.0,
+    "description":
+    "The number of molecular orbitals for the computation."
+}
+
+calcinfo_properties["calcinfo_nalapha"] = {
+    "type":
+    "number",
+    "multipleOf": 1.0,
+    "description":
+    "The number of alpha electrons in the computation."
+}
+calcinfo_properties["calcinfo_nbeta"] = {
+    "type":
+    "number",
+    "multipleOf": 1.0,
+    "description":
+    "The number of beta electrons in the computation."
+}
+
+calcinfo_properties["calcinfo_natom"] = {
+    "type":
+    "number",
+    "multipleOf": 1.0,
+    "description":
+    "The number of atoms in the computation."
+}
+
