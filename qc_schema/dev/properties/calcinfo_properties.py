9355cef08bd60b97b12257102b542453c423edaa
Daniel Smith <malorian@me.com>
2018-04-02 13:11:55 -0400

    Adds 4 example computations

--- qc_schema/dev/properties/calcinfo_properties.py
+++ qc_schema/dev/properties/calcinfo_properties.py
@@ -5,41 +5,31 @@ The complete list of the general computation properties.
 calcinfo_properties = {}
 
 calcinfo_properties["calcinfo_nbasis"] = {
-    "type":
-    "number",
+    "type": "number",
     "multipleOf": 1.0,
-    "description":
-    "The number of basis functions for the computation."
+    "description": "The number of basis functions for the computation."
 }
 
 calcinfo_properties["calcinfo_nmo"] = {
-    "type":
-    "number",
+    "type": "number",
     "multipleOf": 1.0,
-    "description":
-    "The number of molecular orbitals for the computation."
+    "description": "The number of molecular orbitals for the computation."
 }
 
 calcinfo_properties["calcinfo_nalapha"] = {
-    "type":
-    "number",
+    "type": "number",
     "multipleOf": 1.0,
-    "description":
-    "The number of alpha electrons in the computation."
+    "description": "The number of alpha electrons in the computation."
 }
+
 calcinfo_properties["calcinfo_nbeta"] = {
-    "type":
-    "number",
+    "type": "number",
     "multipleOf": 1.0,
-    "description":
-    "The number of beta electrons in the computation."
+    "description": "The number of beta electrons in the computation."
 }
 
 calcinfo_properties["calcinfo_natom"] = {
-    "type":
-    "number",
+    "type": "number",
     "multipleOf": 1.0,
-    "description":
-    "The number of atoms in the computation."
+    "description": "The number of atoms in the computation."
 }
-
