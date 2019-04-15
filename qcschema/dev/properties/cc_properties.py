fe11977525a8251652a4824a19348965aac157ad
Sebastian Lee <sjrl423@gmail.com>
2019-04-15 12:59:26 -0700

    CC Properties: Refactored from ccsd_properties to cc_properties and updated string description in cc_properties.

--- qcschema/dev/properties/cc_properties.py
+++ qcschema/dev/properties/cc_properties.py
@@ -1,43 +1,43 @@
 """
-The complete list of CCSD properties.
+The complete list of Coupled-Cluster properties.
 """
 
-ccsd_properties = {}
+cc_properties = {}
 
-ccsd_properties["ccsd_same_spin_correlation_energy"] = {
+cc_properties["ccsd_same_spin_correlation_energy"] = {
     "type":
     "number",
     "description":
     "The unscaled portion of the CCSD correlation energy from same-spin or triplet doubles correlations."
 }
 
-ccsd_properties["ccsd_opposite_spin_correlation_energy"] = {
+cc_properties["ccsd_opposite_spin_correlation_energy"] = {
     "type":
     "number",
     "description":
     "The unscaled portion of the CCSD correlation energy from opposite-spin or singlet doubles correlations."
 }
 
-ccsd_properties["ccsd_singles_energy"] = {
+cc_properties["ccsd_singles_energy"] = {
     "type": "number",
     "description": "The singles portion of the CCSD correlation energy. Zero except in ROHF."
 }
 
-ccsd_properties["ccsd_doubles_energy"] = {
+cc_properties["ccsd_doubles_energy"] = {
     "type":
     "number",
     "description":
     "The doubles portion of the CCSD correlation energy including same-spin and opposite-spin correlations."
 }
 
-ccsd_properties['ccsd_total_correlation_energy'] = {
+cc_properties['ccsd_total_correlation_energy'] = {
     "type":
     "number",
     "description":
-    "The doubles portion of the CCSD correlation energy including same-spin and opposite-spin correlations."
+    "The total CCSD correlation energy. "
 }
 
-ccsd_properties['ccsd_total_energy'] = {
+cc_properties['ccsd_total_energy'] = {
     "type":
     "number",
     "description":
--- qcschema/dev/properties/properties_base.py
+++ qcschema/dev/properties/properties_base.py
@@ -4,7 +4,7 @@ The base file for QC Schema properties.
 
 from .scf_properties import scf_properties
 from .mp_properties import mp_properties
-from .ccsd_properties import ccsd_properties
+from .cc_properties import cc_properties
 from .calcinfo_properties import calcinfo_properties
 
 properties = {
@@ -17,5 +17,5 @@ properties = {
 # Update new keys
 properties["properties"].update(scf_properties)
 properties["properties"].update(mp_properties)
-properties["properties"].update(ccsd_properties)
+properties["properties"].update(cc_properties)
 properties["properties"].update(calcinfo_properties)
