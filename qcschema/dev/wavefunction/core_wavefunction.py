1c46ecb305aa68529cf6ccf62e4a0a36313824f9
Matthew Welborn <mattgwelborn@gmail.com>
2019-09-23 12:41:01 -0400

    formatting

--- qcschema/dev/wavefunction/core_wavefunction.py
+++ qcschema/dev/wavefunction/core_wavefunction.py
@@ -7,7 +7,7 @@ core_wavefunction = {}
 # core hamiltonian
 core_wavefunction["h_core_a"] = {
     "type": "array",
-    "description": "Alpha-spin core (one-electron) Hamiltonian in the AO basis",
+    "description": "Alpha-spin core (one-electron) Hamiltonian in the AO basis.",
     "items": {"type": "number"},
     "shape": {"nao", "nao"}
 }
@@ -15,7 +15,7 @@ core_wavefunction["h_core_a"] = {
 
 core_wavefunction["h_core_b"] = {
     "type": "array",
-    "description": "Beta-spin core (one-electron) Hamiltonian in the AO basis",
+    "description": "Beta-spin core (one-electron) Hamiltonian in the AO basis.",
     "items": {"type": "number"},
     "shape": {"nao", "nao"}
 }
@@ -24,7 +24,7 @@ core_wavefunction["h_core_b"] = {
 # effective core hamiltonian
 core_wavefunction["h_effective_a"] = {
     "type": "array",
-    "description": "Alpha-spin effective core (one-electron) Hamiltonian in the AO basis",
+    "description": "Alpha-spin effective core (one-electron) Hamiltonian in the AO basis.",
     "items": {"type": "number"},
     "shape": {"nao", "nao"}
 }
@@ -32,7 +32,7 @@ core_wavefunction["h_effective_a"] = {
 
 core_wavefunction["h_effective_b"] = {
     "type": "array",
-    "description": "Beta-spin effective core (one-electron) Hamiltonian in the AO basis",
+    "description": "Beta-spin effective core (one-electron) Hamiltonian in the AO basis.",
     "items": {"type": "number"},
     "shape": {"nao", "nao"}
 }
